c2258ccc02580023dc410c64f800d958e66f0d57
enne11 <1247939131@qq.com>
2021-06-23 21:28:04 +0800

    123123

---
+++ src/views/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <form action=''>
+      用户名：<input type="text" />
+      密码：<input type="text" />
+      <input type="submit" value="登陆">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: "Login",
+    components: {
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  mounted() {},
+  watch: {},
+  methods: {},
+  components: {},
+};
+</script>
+
+<style scoped lang="scss">
+</style>
